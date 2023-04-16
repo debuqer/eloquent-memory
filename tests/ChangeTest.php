1add7d9d4b0e8c4d93eaf96747b5bee160a29097
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-04-16 17:15:52 +0330

    test model rollbacks

--- tests/ChangeTest.php
+++ tests/ChangeTest.php
@@ -1,10 +1,12 @@
 <?php
 use Debuqer\EloquentMemory\ChangeTypes\ModelUpdated;
 use Debuqer\EloquentMemory\ChangeTypes\ModelCreated;
+use Debuqer\EloquentMemory\ChangeTypes\ModelDeleted;
+use Debuqer\EloquentMemory\ChangeTypes\ModelSoftDeleted;
 
 
 /**
- * ModelUpdated Rollback is ModelUpdated
+ * ModelUpdated Rollback
  */
 test('ModelUpdated Rollback ', function () {
     $after = createAPost();
@@ -21,7 +23,7 @@ test('ModelUpdated Rollback ', function () {
 
 
 /**
- * ModelCreated Rollback is ModelDeleted
+ * ModelCreated Rollback
  */
 test('ModelCreated Rollback ', function () {
     $item = createAFakePost();
@@ -29,4 +31,44 @@ test('ModelCreated Rollback ', function () {
 
     expect($c->getRollbackChange()->getType())->toBe('model-deleted');
     expect($c->getRollbackChange()->getAttributes())->toBe($item->getRawOriginal());
+});
+
+/**
+ * ModelDeleted Rollback
+ */
+test('ModelDeleted Rollback ', function () {
+    $item = createAPost();
+    $c = new ModelDeleted(get_class($item), $item->getRawOriginal());
+
+    expect($c->getRollbackChange()->getType())->toBe('model-created');
+    expect($c->getRollbackChange()->getAttributes())->toBe($item->getRawOriginal());
+});
+
+/**
+ * ModelSoftDeleted Rollback
+ */
+test('ModelSoftDeleted Rollback ', function () {
+    $before = createAPost();
+    $after = (clone $before);
+    $after->delete();
+
+    $c = new ModelSoftDeleted(get_class($after), $before->getRawOriginal(), $after->getRawOriginal());
+    expect($c->getRollbackChange()->getType())->toBe('model-restored');
+    expect($c->getRollbackChange()->getAfterAttributes())->toBe( $before->getRawOriginal());
+    expect($c->getRollbackChange()->getBeforeAttributes())->toBe( $after->getRawOriginal());
+});
+
+
+/**
+ * ModelRestored Rollback
+ */
+test('ModelRestored Rollback ', function () {
+    $before = createAPost();
+    $after = (clone $before);
+    $after->delete();
+
+    $c = new ModelSoftDeleted(get_class($after), $before->getRawOriginal(), $after->getRawOriginal());
+    expect($c->getRollbackChange()->getType())->toBe('model-restored');
+    expect($c->getRollbackChange()->getAfterAttributes())->toBe( $before->getRawOriginal());
+    expect($c->getRollbackChange()->getBeforeAttributes())->toBe( $after->getRawOriginal());
 });
