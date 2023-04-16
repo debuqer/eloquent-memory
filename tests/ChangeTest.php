55462e2da3fce58ad70bd76593270df0f1aa0f4b
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-04-16 17:11:53 +0330

    test for rollback

--- tests/ChangeTest.php
+++ tests/ChangeTest.php
@@ -1,10 +1,12 @@
 <?php
 use Debuqer\EloquentMemory\ChangeTypes\ModelUpdated;
+use Debuqer\EloquentMemory\ChangeTypes\ModelCreated;
+
 
 /**
- * ModelCreated Rollback
+ * ModelUpdated Rollback is ModelUpdated
  */
-test('11', function () {
+test('ModelUpdated Rollback ', function () {
     $after = createAPost();
     $before = (clone $after);
     $before->update([
@@ -12,4 +14,19 @@ test('11', function () {
     ]);
 
     $c = new ModelUpdated(get_class($after), $before->getRawOriginal(), $after->getRawOriginal());
+    expect($c->getRollbackChange()->getType())->toBe('model-updated');
+    expect($c->getRollbackChange()->getAfterAttributes())->toBe( $before->getRawOriginal());
+    expect($c->getRollbackChange()->getBeforeAttributes())->toBe( $after->getRawOriginal());
+});
+
+
+/**
+ * ModelCreated Rollback is ModelDeleted
+ */
+test('ModelCreated Rollback ', function () {
+    $item = createAFakePost();
+    $c = new ModelCreated(get_class($item), $item->getRawOriginal());
+
+    expect($c->getRollbackChange()->getType())->toBe('model-deleted');
+    expect($c->getRollbackChange()->getAttributes())->toBe($item->getRawOriginal());
 });
