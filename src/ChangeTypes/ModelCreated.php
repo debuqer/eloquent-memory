660cbeecc966d390a2352e7a4cfd41facfee413a
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-04-18 14:25:39 +0330

    removed not necessary ises

--- src/ChangeTypes/ModelCreated.php
+++ src/ChangeTypes/ModelCreated.php
@@ -4,19 +4,10 @@
 namespace Debuqer\EloquentMemory\ChangeTypes;
 
 
-use Debuqer\EloquentMemory\ChangeTypes\Checkers\ItemIsModel;
 use Debuqer\EloquentMemory\ChangeTypes\Checkers\ItemExists;
-use Debuqer\EloquentMemory\ChangeTypes\Checkers\ItemIsNotModel;
-use Debuqer\EloquentMemory\ChangeTypes\Checkers\ItemIsNotNull;
-use Debuqer\EloquentMemory\ChangeTypes\Checkers\ItemIsNotTrash;
 use Debuqer\EloquentMemory\ChangeTypes\Checkers\ItemIsNull;
-use Debuqer\EloquentMemory\ChangeTypes\Checkers\ItemNotExists;
-use Debuqer\EloquentMemory\ChangeTypes\Checkers\ItemsAreNotTheSameType;
-use Debuqer\EloquentMemory\ChangeTypes\Checkers\ItemsAreTheSameType;
 use Debuqer\EloquentMemory\ChangeTypes\Concerns\HasAttributes;
 use Debuqer\EloquentMemory\ChangeTypes\Concerns\HasModelClass;
-use Debuqer\EloquentMemory\Tests\Fixtures\Post;
-use Illuminate\Database\Eloquent\Model;
 
 class ModelCreated extends BaseChangeType implements ChangeTypeInterface
 {
