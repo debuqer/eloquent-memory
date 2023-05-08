a677912719cb073cd34be2887b488b52c2270dd2
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-05-08 09:11:28 +0330

    add test for model updated

--- tests/ModelUpdatedTest.php
+++ tests/ModelUpdatedTest.php
@@ -2,17 +2,28 @@
 use Debuqer\EloquentMemory\Tests\Fixtures\Post;
 use Debuqer\EloquentMemory\ChangeTypes\ModelUpdated;
 
-test('ModelUpdated::up will update a model in database with given attributes', function () {
-    $after = createAPost();
-    $before = (clone $after);
-    $before->update([
-        'title' => 'Title changed!'
+beforeEach(function () {
+    $before = createAPost();
+    $after = (clone $before);
+    $after->update([
+        'title' => 'Title changed!',
+        'json' => ['new json']
     ]);
+    $this->before = $before;
+    $this->after = $after;
 
-    $c = new ModelUpdated(get_class($after), $after->getKey(), $before->getRawOriginal(), $after->getRawOriginal());
-    $c->up();
+    // reset to state before updating
+    $after->delete();
+    (new Post())->setRawAttributes($before->getRawOriginal())->save();
 
-    expect(Post::find($after->getKey())->title)->toBe($after->title);
+    $this->c = ModelUpdated::createFromModel($before, $after);
+});
+
+test('ModelUpdated::up will update a model in database with given attributes', function () {
+    $this->c->up();
+
+    expect(Post::first()->title)->toBe($this->after->title);
+    expect(Post::first()->json)->toBe($this->after->json);
 });
 
 test('ModelUpdated::getRollbackChange returns instanceof ModelUpdated with reversed properties', function () {
