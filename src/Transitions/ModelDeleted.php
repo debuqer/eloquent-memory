eba59312e0eafcc9fe322745d6eda6c8226f320b
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-17 12:25:55 +0330

    remove unused

--- src/Transitions/ModelDeleted.php
+++ src/Transitions/ModelDeleted.php
@@ -4,13 +4,8 @@
 namespace Debuqer\EloquentMemory\Transitions;
 
 
-use Debuqer\EloquentMemory\StorageModels\TransitionStorageModelContract;
 use Debuqer\EloquentMemory\Transitions\Concerns\HasAttributes;
-use Debuqer\EloquentMemory\Transitions\Concerns\HasModelClass;
-use Debuqer\EloquentMemory\Transitions\Concerns\HasModelKey;
-use Debuqer\EloquentMemory\Transitions\Concerns\HasOldAttributes;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
 class ModelDeleted extends BaseTransition implements TransitionInterface
 {
--- src/Transitions/ModelUpdated.php
+++ src/Transitions/ModelUpdated.php
@@ -4,14 +4,8 @@
 namespace Debuqer\EloquentMemory\Transitions;
 
 
-use Debuqer\EloquentMemory\StorageModels\TransitionStorageModelContract;
 use Debuqer\EloquentMemory\Transitions\Concerns\HasAttributes;
-use Debuqer\EloquentMemory\Transitions\Concerns\HasModelKey;
-use Debuqer\EloquentMemory\Transitions\Concerns\HasOldAttributes;
-use Debuqer\EloquentMemory\Transitions\Concerns\HasModelClass;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
-use PhpParser\Node\Expr\AssignOp\Mod;
 
 class ModelUpdated extends BaseTransition implements TransitionInterface
 {
